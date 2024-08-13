9d777e4df1009f9004a296d8aa87425dabc694f6
Shahnewaj-786 <shahnewajshaikh786@gmail.com>
2024-08-14 00:28:52 +0600

    Basic While Loop

---
+++ Node  JS/while_loop.js
@@ -0,0 +1,15 @@
+//Basic While Loop
+
+const number = []
+number.push(10)
+number.push(100)
+number.push(1000)
+
+i=0;
+while(
+    i<number.length
+){
+    console.log("Print Using While Loop: " + number[i]);
+    i++
+    //i=i+4;
+}
