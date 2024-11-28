0bfa36a14d1b1081467c48e0bb0e4ed655b51f07
Shahnewaj-786 <shahnewajshaikh786@gmail.com>
2024-11-29 01:01:17 +0600

    add watchForFileChaneg to false fro turn of cypress auto run

--- Cypress/TestProject_CURA/cypress.config.js
+++ Cypress/TestProject_CURA/cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  watchForFileChanges: false, //use watch for file change to turn of auto run on cypress
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- Cypress/TestProject_CURA/cypress/e2e/login.cy.js
+++ Cypress/TestProject_CURA/cypress/e2e/login.cy.js
@@ -1,6 +1,10 @@
 describe("Login Functionality",()=>{
     it("Valid Login", ()=>{
-        cy.visit("https://katalon-demo-cura.herokuapp.com/");       
+        cy.visit("https://katalon-demo-cura.herokuapp.com/"); 
+        cy.get('#btn-make-appointment').click();     
+        cy.get('#txt-username') 
+        cy.get('#txt-password')
+        cy.get('#btn-login').click();
     })
 
 })
