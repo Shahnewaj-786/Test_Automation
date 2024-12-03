e296087427dd4d5b59d7ec361a653b1199ad3523
Shahnewaj-786 <shahnewajshaikh786@gmail.com>
2024-12-04 00:27:32 +0600

    Basic Add Function

---
+++ Node  JS/operationalFunction.js
@@ -0,0 +1,10 @@
+// define a function to add two numbers
+
+function add (a,b){
+    return a+b;
+}
+
+
+const result = add (5,20);
+console.log(`The sum is: ${result}`); // (`The sum is: ${result}`) --> ` can be used for print variable with text
+
