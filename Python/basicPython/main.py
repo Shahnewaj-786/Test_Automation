608af6fe5193df0632ff11ccfbab346b663ffed7
Shahnewaj-786 <shahnewajshaikh786@gmail.com>
2025-02-03 23:09:53 +0600

    add string and variables

--- Python/basicPython/main.py
+++ Python/basicPython/main.py
@@ -1,9 +1,14 @@
 x = 100
 y = "Alice"
 c = 67.98
-
 a,b,d = 25,"New",25.3
 
 print(d)
 print(y)
 print(type(c))
+
+name = "Sheikh"
+cg: int = 3
+phone = "+8801621720045"
+
+print("My name is " + name + " My CG is ",cg, "You can call me to " + phone)
